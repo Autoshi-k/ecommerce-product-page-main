63735bba9ef5d6f74ef05d74cdd0d639b858c524
Autoshi-K <autoshigit@gmail.com>
2021-10-20 21:39:51 -0900

    cart window created

--- js/createCartPop.js
+++ js/createCartPop.js
@@ -14,22 +14,32 @@ function createImg(addClass, addSrc, addAlt) {
   return newElement;
 }
 
-function createCart () {
-  const divCart = createEl('div', 'dom-cart');
-  divCart.append(createEl('h3', undefined, 'Cart'));
-  
+function cartEmpty () {
+  return createEl('div', 'dom-cart-empty', 'Your cart is empy');
+}
+
+function cartItemInfo (items) {
   const cartContainer = createEl('div', 'dom-cart-container');
   cartContainer.append(createImg('dom-img', 'images/image-product-1-thumbnail.jpg', 'product'));
   
   const domItemInfo = createEl('div', 'dom-items-info');
   domItemInfo.append(createEl('p', undefined, 'Fall Limited Edition Sneakers'));
-  domItemInfo.append(createEl('p', undefined, '$125.00 x 3 <span>$375.00</span>'));
+  domItemInfo.append(createEl('p', undefined, `$125.00 x ${items} <span>$${(items * 125)}.00</span>`));
   cartContainer.append(domItemInfo);
   
   cartContainer.append(createImg('dom-remove', 'images/icon-delete.svg', 'remove item'));
-  
-  divCart.append(cartContainer);
-  divCart.append(createEl('button', 'dom-checkout', 'Checkout'));
+
+  return cartContainer;
+}
+
+function createCart () {
+  const divCart = createEl('div', 'dom-cart');
+  divCart.append(createEl('h3', undefined, 'Cart'));
+
+  if (itemInCart) {
+    divCart.append(cartItemInfo(itemInCart));
+    divCart.append(createEl('button', 'dom-checkout', 'Checkout'));
+  } else divCart.append(cartEmpty());
   
   document.querySelector('header').append(divCart);
 }
